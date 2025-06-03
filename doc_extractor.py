5c828d63880ae3c25667aa6bc1f32e3971b2137b
thiago.weidman <tw@weidman.com.br>
2025-06-03 19:57:53 -0300

    Oracle added 2FA with just cell phone validation :( Adding sleeps to allow me to type the 2fa code

--- doc_extractor.py
+++ doc_extractor.py
@@ -7,7 +7,8 @@ from selenium.webdriver.firefox.options import Options
 import os
 from dotenv import load_dotenv
 
-import time
+
+from time import sleep, time
 
 load_dotenv()
 
@@ -15,22 +16,28 @@ load_dotenv()
 file_path = os.path.join(os.getcwd(), "func_docs")
 os.makedirs(file_path, exist_ok=True)
 
+
 # Função para esperar que todos os downloads sejam concluídos
-def wait_for_downloads(directory, timeout=60, poll_interval=1):
+def wait_for_downloads(directory, timeout=3600, poll_interval=1):
     """
     Espera que todos os downloads na pasta sejam concluídos.
-    
+
     :param directory: Caminho para o diretório de downloads.
     :param timeout: Tempo máximo de espera em segundos.
     :param poll_interval: Intervalo entre verificações em segundos.
     """
-    end_time = time.time() + timeout
-    while time.time() < end_time:
+    end_time = time() + timeout
+    while time() < end_time:
         # Verifica se existem arquivos temporários (exemplo: .crdownload ou .part)
-        if not any(file.endswith(".crdownload") or file.endswith(".part") for file in os.listdir(directory)):
+        if not any(
+            file.endswith(".crdownload") or file.endswith(".part")
+            for file in os.listdir(directory)
+        ):
             return True
-        time.sleep(poll_interval)
-    raise TimeoutError("Os downloads não foram concluídos dentro do tempo especificado.")
+        sleep(poll_interval)
+    raise TimeoutError(
+        "Os downloads não foram concluídos dentro do tempo especificado."
+    )
 
 
 # Wait until the page is fully loaded
@@ -48,17 +55,20 @@ def main():
     if mos_user is None or mos_pass is None:
         print("Setar senha e pass!")
         return False
-    
+
     # Inicializa o WebDriver
     firefox_options = Options()
-    firefox_options.set_preference('browser.download.folderList', 2)
-    firefox_options.set_preference('browser.download.manager.showWhenStarting', False)
-    firefox_options.set_preference('browser.download.dir', file_path)
-    firefox_options.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream,application/pdf')
+    firefox_options.set_preference("browser.download.folderList", 2)
+    firefox_options.set_preference("browser.download.manager.showWhenStarting", False)
+    firefox_options.set_preference("browser.download.dir", file_path)
+    firefox_options.set_preference(
+        "browser.helperApps.neverAsk.saveToDisk",
+        "application/octet-stream,application/pdf",
+    )
     firefox_options.set_preference("pdfjs.disabled", True)
 
     driver = webdriver.Firefox(options=firefox_options)
-    
+
     try:
         # Abre a página de login
         url = "https://support.oracle.com/epmos/faces/DocumentDisplay?id=1585843.1"
@@ -68,33 +78,41 @@ def main():
         wait = WebDriverWait(driver, 60)  # Ajuste o tempo se necessário
 
         # Localiza e preenche o campo de login
-        #username_field = driver.find_element(By.ID, "idcs-signin-basic-signin-form-username")
-        username_field = wait.until(EC.visibility_of_element_located((By.ID, "idcs-signin-basic-signin-form-username")))
+        # username_field = driver.find_element(By.ID, "idcs-signin-basic-signin-form-username")
+        username_field = wait.until(
+            EC.visibility_of_element_located(
+                (By.ID, "idcs-signin-basic-signin-form-username")
+            )
+        )
         username_field.send_keys(mos_user)  # Substitua por seu nome de usuário
         username_field.send_keys(Keys.RETURN)
 
         # Localiza e preenche o campo de senha
-        #password_field = driver.find_element(By.ID, "idcs-auth-pwd-input|input")
-        password_field = wait.until(EC.visibility_of_element_located((By.ID, "idcs-auth-pwd-input|input")))
+        # password_field = driver.find_element(By.ID, "idcs-auth-pwd-input|input")
+        password_field = wait.until(
+            EC.visibility_of_element_located((By.ID, "idcs-auth-pwd-input|input"))
+        )
         password_field.send_keys(mos_pass)  # Substitua por sua senha
 
         # Envia o formulário de login
         password_field.send_keys(Keys.RETURN)
 
+        sleep(60)
         # Wait page load
         print("Waiting first element to load...")
         wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "xq1")))
         print("Waiting for the page to fully load...")
         wait_for_page_load(driver)
         print("done...")
-        
+        sleep(10)
         elems = driver.find_elements(by=By.XPATH, value="//a[@href]")
         href_links = [e.get_attribute("href") for e in elems]
         print("Start downloading...")
         for i in href_links:
-            if i.__contains__('downloadattachmentprocessor'):
+            if i.__contains__("downloadattachmentprocessor"):
                 print(i)
                 driver.execute_script(f"window.open('{i}')")
+                sleep(0.5)
 
         wait_for_downloads(file_path)
 
@@ -105,7 +123,6 @@ def main():
         # Fecha o navegador
         driver.quit()
         pass
-
 
 
 if __name__ == "__main__":
--- url_extract.py
+++ url_extract.py
@@ -1,13 +1,16 @@
 import os
 import requests
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
+from time import sleep
 
 # Cabeçalhos para simular um navegador
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
 
-def download_pdfs(url,folder_name="mom_func_docs"):
+
+def download_pdfs(url, folder_name="mom_func_docs"):
     # Diretório para salvar os PDFs
     output_dir = os.path.join(os.getcwd(), folder_name)
     os.makedirs(output_dir, exist_ok=True)
@@ -15,42 +18,69 @@ def download_pdfs(url,folder_name="mom_func_docs"):
         # Solicitação da página
         response = requests.get(url, headers=headers)
         response.raise_for_status()
-        
+
         # Parsear o conteúdo HTML
         soup = BeautifulSoup(response.text, "html.parser")
-        
+
         # Encontrar todos os links com extensão .pdf
         pdf_links = soup.find_all("a", href=lambda href: href and href.endswith(".pdf"))
-        
+
         print(f"Encontrados {len(pdf_links)} PDFs.")
-        
+
         for link in pdf_links:
             pdf_url = link["href"]
+
             # Resolver URLs relativas
             if not pdf_url.startswith("http"):
-                pdf_url = requests.compat.urljoin(url, pdf_url)
-            
+                pdf_url = urljoin(url, pdf_url)
+
             # Nome do arquivo
             filename = os.path.join(output_dir, pdf_url.split("/")[-1])
-            
+
             # Baixar o PDF
             print(f"Baixando: {pdf_url}")
-            pdf_response = requests.get(pdf_url, headers=headers)
-            pdf_response.raise_for_status()
-            
+            try:
+                pdf_response = requests.get(pdf_url, headers=headers)
+                pdf_response.raise_for_status()
+            except Exception as e:
+                print(f"Erro ao baixar o PDF: {e}")
+                if pdf_links.count(link) < 5:
+                    pdf_links.append(link)
+                continue
+
             with open(filename, "wb") as pdf_file:
                 pdf_file.write(pdf_response.content)
-            
+
             print(f"Salvo: {filename}")
+            # sleep to avoid connection to be blocked
+            sleep(1)
     except Exception as e:
         print(f"Erro: {e}")
 
 
 if __name__ == "__main__":
     # Executar o download
-    download_pdfs("https://docs.oracle.com/en/industries/retail/retail-merchandising-foundation-cloud/latest/books.html","mfcs_docs")
-    download_pdfs("https://docs.oracle.com/en/industries/retail/retail-pricing-cloud/latest/books.html","rpm_docs")
-    download_pdfs("https://docs.oracle.com/en/industries/retail/retail-invoice-matching-cloud/latest/books.html","reim_docs")
-    download_pdfs("https://docs.oracle.com/en/industries/retail/retail-integration-cloud/latest/books.html","int_docs")
-    download_pdfs("https://docs.oracle.com/en/industries/retail/retail-fiscal-management/latest/books.html","rfm_docs")
-    download_pdfs("https://docs.oracle.com/en/industries/retail/retail-allocation-cloud/latest/books.html","alloc_docs")
+    download_pdfs(
+        "https://docs.oracle.com/en/industries/retail/retail-merchandising-foundation-cloud/latest/books.html",
+        "mfcs_docs",
+    )
+    download_pdfs(
+        "https://docs.oracle.com/en/industries/retail/retail-pricing-cloud/latest/books.html",
+        "rpm_docs",
+    )
+    download_pdfs(
+        "https://docs.oracle.com/en/industries/retail/retail-invoice-matching-cloud/latest/books.html",
+        "reim_docs",
+    )
+    download_pdfs(
+        "https://docs.oracle.com/en/industries/retail/retail-integration-cloud/latest/books.html",
+        "int_docs",
+    )
+    download_pdfs(
+        "https://docs.oracle.com/en/industries/retail/retail-fiscal-management/latest/books.html",
+        "rfm_docs",
+    )
+    download_pdfs(
+        "https://docs.oracle.com/en/industries/retail/retail-allocation-cloud/latest/books.html",
+        "alloc_docs",
+    )
