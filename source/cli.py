551383777e963dae87f14128f90c097d3388b635
thiago.weidman <tw@weidman.com.br>
2025-06-25 13:58:51 -0300

    added click options to select to run just auth or not auth docs

--- source/cli.py
+++ source/cli.py
@@ -1,37 +1,38 @@
 # -*- coding: utf-8 -*-
 
+import sys
+import threading
 
 import click
-from doc_extractor import download_docs
+from doc_extractor import download_docs, open_driver
 from logger import logger
 from url_extract import download_pdfs
 
 
 @click.command()
-# @click.option("-s", "--start_month", type=int, default=1)
-# @click.argument("year", type=int, default=date.today().year)
-def get_docs():  # (year, start_month):
+@click.option("-a", "--auth_docs", is_flag=True)
+@click.option("-n", "--no_auth_docs", is_flag=True)
+def get_docs(auth_docs, no_auth_docs):  # (year, start_month):
+    is_both = not (auth_docs or no_auth_docs)
     logger.info("Starting from CLI")
-    # TODO: thread this so both can run in parallel
-    # infos that need auth
-    download_docs(
-        [
-            # Merch functional docs
-            "https://support.oracle.com/epmos/faces/DocumentDisplay?id=1585843.1",
-            # Extensions docs
-            "https://support.oracle.com/epmos/faces/DocumentDisplay?id=2978473.1",
-            # Rics func docs
-            "https://support.oracle.com/epmos/faces/DocumentDisplay?id=2643542.1",
-            # RDS func docs
-            "https://support.oracle.com/epmos/faces/DocumentDisplay?id=2899701.1",
-            # POM func docs
-            "https://support.oracle.com/epmos/faces/DocumentDisplay?id=2815461.1",
-            # Localization func docs
-            "https://support.oracle.com/epmos/faces/DocumentDisplay?id=2534504.2",
-            # blueprint func docs
-            "https://support.oracle.com/epmos/faces/DocumentDisplay?id=2677553.1",
-        ]
-    )
+
+    # TODO: transform the sources into a config file
+    auth_sources = [
+        # Merch functional docs
+        "https://support.oracle.com/epmos/faces/DocumentDisplay?id=1585843.1",
+        # Extensions docs
+        "https://support.oracle.com/epmos/faces/DocumentDisplay?id=2978473.1",
+        # Rics func docs
+        "https://support.oracle.com/epmos/faces/DocumentDisplay?id=2643542.1",
+        # RDS func docs
+        "https://support.oracle.com/epmos/faces/DocumentDisplay?id=2899701.1",
+        # POM func docs
+        "https://support.oracle.com/epmos/faces/DocumentDisplay?id=2815461.1",
+        # Localization func docs
+        "https://support.oracle.com/epmos/faces/DocumentDisplay?id=2534504.2",
+        # blueprint func docs
+        "https://support.oracle.com/epmos/faces/DocumentDisplay?id=2677553.1",
+    ]
 
     # no auth sources
     sources = {
@@ -43,8 +44,25 @@ def get_docs():  # (year, start_month):
         "mfcs_docs": "https://docs.oracle.com/en/industries/retail/retail-merchandising-foundation-cloud/latest/books.html",
     }
 
-    # for source in sources:
-    download_pdfs(sources)
+    # infos that need auth
+    if auth_docs or is_both:
+        driver = open_driver()
+        if driver is None:
+            sys.exit(1)
+        thread_docs_auth = threading.Thread(
+            target=download_docs, args=(auth_sources, driver)
+        )
+        thread_docs_auth.start()
+
+    if no_auth_docs or is_both:
+        thread_docs_noauth = threading.Thread(target=download_pdfs, args=(sources,))
+        thread_docs_noauth.start()
+
+    # Wait for threads to complete
+    if auth_docs or is_both:
+        thread_docs_auth.join()
+    if no_auth_docs or is_both:
+        thread_docs_noauth.join()
 
 
 if __name__ == "__main__":
